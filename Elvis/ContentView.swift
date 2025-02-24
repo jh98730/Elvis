66e4029a107e915525b4205f6d1ba6d9e6a2ad3c
John Howard <john.k.howard@gmail.com>
2025-02-24 14:53:00 -0500

    Elvis intial

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var imageView = ""
+    @State private var message = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            Spacer()
+            
+            Image(systemName: imageView)
+                .resizable().scaledToFit()
+                .frame(width: 300, height: 300)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    imageView = "peacesign"
+                    message = "Peace"
+                }
+                Button("Love") {
+                    imageView = "heart"
+                    message = "Love"
+                }
+                Button("Enlightenment") {
+                    imageView = "lightbulb"
+                    message = "Enlightenment"
+                }
+            }
+            .tint(.purple)
+            .buttonStyle(.borderedProminent)
+            
+            
+                
         }
         .padding()
     }
