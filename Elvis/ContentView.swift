fd963d3e01f0eb77dfc5511e2db2bc87714ec050
John Howard <john.k.howard@gmail.com>
2025-02-24 14:15:43 -0500

    Initial Commit

---
+++ Elvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis
+//
+//  Created by John Howard on 2/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by John Howard on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
